655d541802d8b7a40ecc11cff424df9e19d37df7
Joy <github.joysaha@gmail.com>
2024-03-26 23:20:53 +0600

    react chart added and fetch data from local storage and show data inforamtion in chart dynamically

--- src/Pages/PageRad/PageRead.jsx
+++ src/Pages/PageRad/PageRead.jsx
@@ -1,52 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from 'recharts';
+import { getDataFromLocal } from '../../utilites/LocalStorage';
 
-const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
 
-const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
 
 const getPath = (x, y, width, height) => {
   return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
@@ -62,7 +18,12 @@ const TriangleBar = (props) => {
 };
 
 export default function App() {
+const [readBookList, setReadBookList] = useState([]);
 
+useEffect(() => {
+    const readdata = getDataFromLocal();
+    setReadBookList(readdata)
+}, [])
     
 const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
 
@@ -119,7 +80,7 @@ const data = [
         <BarChart
       width={600}
       height={400}
-      data={data}
+      data={readBookList}
       margin={{
         top: 20,
         right: 30,
@@ -128,9 +89,9 @@ const data = [
       }}
     >
       <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
+      <XAxis dataKey="bookName" />
       <YAxis />
-      <Bar dataKey="uv" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
+      <Bar dataKey="totalPages" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
         {data.map((entry, index) => (
           <Cell key={`cell-${index}`} fill={colors[index % 20]} />
         ))}
