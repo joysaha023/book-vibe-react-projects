9ccb75229f69ad2e6d459f722013fdfd4710515b
Joy <github.joysaha@gmail.com>
2024-03-27 23:06:29 +0600

    final and last final commit

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,9 +2,7 @@ import React from 'react';
 import Banner from '../../components/Banner/Banner';
 import BookCardHome from '../../components/BookCardHome/BookCardHome';
 
-
-const Home = () => {
-    
+const Home = () => {  
     return (
         <div>
             <Banner></Banner>
